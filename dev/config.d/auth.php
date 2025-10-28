537656589c717abc65bb343b85c932390882d987
cultdigitaludia <cultdigitalsmct@gmail.com>
2025-10-28 17:12:35 +0000

    feat: Configura login social Google e adiciona plugin AccountStatus

--- dev/config.d/auth.php
+++ dev/config.d/auth.php
@@ -1,33 +1,45 @@
 <?php
+// Define a URL base do seu site. Isto irá construir a URL completa dinamicamente.
+define('BASE_URL', (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST']);
 
 return [
-    'auth.provider' => 'Fake',
-    // 'auth.provider' => '\MultipleLocalAuth\Provider',
-    // 'auth.config' => [
-    //     'salt' => env('AUTH_SALT', null),
-    //     'timeout' => '24 hours',
-    //     'strategies' => [
-    //         'Facebook' => [
-    //             'app_id' => env('AUTH_FACEBOOK_APP_ID', null),
-    //             'app_secret' => env('AUTH_FACEBOOK_APP_SECRET', null),
-    //             'scope' => env('AUTH_FACEBOOK_SCOPE', 'email'),
-    //         ],
-    //         'LinkedIn' => [
-    //             'api_key' => env('AUTH_LINKEDIN_API_KEY', null),
-    //             'secret_key' => env('AUTH_LINKEDIN_SECRET_KEY', null),
-    //             'redirect_uri' => '/autenticacao/linkedin/oauth2callback',
-    //             'scope' => env('AUTH_LINKEDIN_SCOPE', 'r_emailaddress')
-    //         ],
-    //         'Google' => [
-    //             'client_id' => env('AUTH_GOOGLE_CLIENT_ID', null),
-    //             'client_secret' => env('AUTH_GOOGLE_CLIENT_SECRET', null),
-    //             'redirect_uri' => '/autenticacao/google/oauth2callback',
-    //             'scope' => env('AUTH_GOOGLE_SCOPE', 'email'),
-    //         ],
-    //         'Twitter' => [
-    //             'app_id' => env('AUTH_TWITTER_APP_ID', null),
-    //             'app_secret' => env('AUTH_TWITTER_APP_SECRET', null),
-    //         ],
-    //     ]
-    // ]
+    'auth.provider' => '\MultipleLocalAuth\Provider',
+
+    'auth.config' => [
+        'salt' => env('AUTH_SALT', 'mude-este-valor-para-algo-aleatorio-e-muito-longo'),
+
+        // NOVO: Diz explicitamente quais logins sociais devem aparecer.
+        'enabled_strategies' => [
+            'Google' => true,
+            // 'Facebook' => true, // Pode descomentar isto mais tarde se configurar o Facebook
+        ],
+        
+        // Estratégias de autenticação social com as suas chaves
+        'strategies' => [
+            'Google' => [
+                'client_id' => env('AUTH_GOOGLE_CLIENT_ID', null),
+                'client_secret' => env('AUTH_GOOGLE_CLIENT_SECRET', null),
+                'redirect_uri' => 'http://localhost/autenticacao/google/oauth2callback',
+                'scope' => 'email profile'
+            ],
+            
+            // 'Facebook' => [
+            //     'app_id' => env('AUTH_FACEBOOK_APP_ID', null),
+            //     'app_secret' => env('AUTH_FACEBOOK_APP_SECRET', null), 
+            //     'scope' => 'email'
+            // ],
+        ],
+
+        // Todas as outras configurações que já tinha (regras de senha, etc.)
+        'timeout' => '24 hours',
+        'enableLoginByCPF' => true,
+        'passwordMustHaveCapitalLetters' => true,
+        'passwordMustHaveLowercaseLetters' => true,
+        'passwordMustHaveSpecialCharacters' => true,
+        'passwordMustHaveNumbers' => true,
+        'minimumPasswordLength' => 6,
+        'sessionTime' => 7200,
+        'numberloginAttemp' => '5', 
+        'timeBlockedloginAttemp' => '900',
+    ]
 ];
--- docker/common/config.d/plugins.php
+++ docker/common/config.d/plugins.php
@@ -5,5 +5,18 @@ return [
         'MultipleLocalAuth' => [ 'namespace' => 'MultipleLocalAuth' ],
         'SamplePlugin' => ['namespace' => 'SamplePlugin'],
         'SpamDetector',
-    ]
+        'AccountStatus' => [
+            'namespace' => 'AccountStatus',
+                "config" => [
+                    'inactive_seal_id' => env('USR_STATUS_INACTIVE_SEAL_ID', 5),
+                    'inactive_period' => env('USR_STATUS_INACTIVE_PERIOD', '-1 year'),
+                    'updated_seal_id' => env('USR_STATUS_UPDATED_SEAL_ID', 4),
+                    'update_expiration_period' => env('USR_STATUS_LAST_UPDATE', '+1 year'),
+                    'update_fields' => [
+                        'name',
+                        'shortDescription',
+                        ],
+                ]
+            ]
+        ]
 ];
